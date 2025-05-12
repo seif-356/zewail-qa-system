76928a40fca146c32b32b7bfffc455bb0addf6bc
seif-356 <82326054+seif-356@users.noreply.github.com>
2025-05-12 21:22:35 +0300

    Delete model directory

--- model/qa_model.py
+++
@@ -1,9 +0,0 @@
-from model.embedder import get_embedding, get_all
-from sentence_transformers.util import cos_sim
-
-def find_best_answer(user_question):
-    questions, embeddings, answers = get_all()
-    user_embedding = get_embedding(user_question)
-    similarities = cos_sim(user_embedding, embeddings)[0]
-    best_idx = similarities.argmax().item()
-    return answers[best_idx]
